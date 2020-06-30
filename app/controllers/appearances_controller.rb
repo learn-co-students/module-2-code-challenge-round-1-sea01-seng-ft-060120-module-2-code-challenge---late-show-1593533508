95d21e6a24da717d0dce03db579f0fd332af2242
btbacker <btbacker@gmail.com>
2020-06-30 15:57:21 -0700

    Code Challenge #1 Mod 2

---
+++ app/controllers/appearances_controller.rb
@@ -0,0 +1,19 @@
+class AppearancesController < ApplicationController
+    def new
+        @appearance = Appearance.new
+    end
+
+    def create
+        @appearance = Appearance.create(appearance_params)
+        if @appearance.save
+            redirect_to episode_path(@appearance.episode_id)
+        else
+            flash[:message]
+            render :new
+    end
+
+private
+    def appearance_params
+        params.require(:appearance).permit(:episode_id, :guest_id, :rating)
+    end
+end
